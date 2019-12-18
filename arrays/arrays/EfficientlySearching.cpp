3d41924f5935345b87fae76fd52f84f00f5563ff
fdo <Fabrizio.Donati@sauber-group.com>
2019-12-18 18:01:50 +0100

    some changes

--- arrays/arrays/EfficientlySearching.cpp
+++ arrays/arrays/EfficientlySearching.cpp
@@ -17,8 +17,8 @@ public:
 constexpr int kNotFound = -1; // use whenever a const integer is required (at compile time and not at runtime)
 
 template <typename T>
-int Search(T element, const T v, int size) {
-  for (int = 0; i < size; i++) {
+int Search(T element, const T* v, int size) {
+  for (int i = 0; i < size; i++) {
     if (v[i] == element) {
       return i;
     }
@@ -33,4 +33,22 @@ void Print(const vector<T>& v) {
     cout << x << ' ';
   }
   cout << " ]\n";
+}
+
+int main() {
+  vector<int> v{33, 44, 55, 11, 22};
+  Print(v);
+
+  cout << "Element to search? ";
+  int x;
+  cin >> x;
+  cout << "\n";
+
+  int pos = Search(x, v.data(), v.size());
+  if (pos == kNotFound) {
+    cout << "element not found!";
+  }
+  else {
+    cout << "element found at index " << pos << endl;
+  }
 }
